a144e233d79d5e69f760a100ffd3df9f94099464
P-ertho <ibisi@frro.utn.edu.ar>
2021-05-17 23:04:39 -0300

    .

---
+++ Crc/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace Crc
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            // Save the document...
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+
+
+            //
+            var path = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "foo.txt");
+            string text = System.IO.File.ReadAllText(@path);
+            
+
+            foreach (string value in args)
+            {
+              // Console.WriteLine(value);
+                String banana = CalculaCRC(value, Encoding.UTF8);
+                //String banana = value;
+                System.IO.File.WriteAllText(@path, banana);
+            }
+
+        }
+
+        static String CalculaCRC(String value, Encoding enc)
+        {
+            byte[] data = enc.GetBytes(value);
+            int crcValue = 0xFFFF;
+            for ( int b = 0; b <data.Length; b++)
+            {
+                for(int i = 0; i < 8; i++)
+                {
+                    Boolean bit = (((int)((uint) data[b] >> (7 - i))) & 1) == 1;
+                    Boolean c15 = (crcValue >> 15 & 1) == 1;
+                    crcValue <<= 1;
+                   if (c15 ^ bit)
+                    {
+                        crcValue ^= 0x1021;
+                    }
+                    
+                }
+                
+            }
+            crcValue &= 0xffff;
+            int hexString =  (crcValue & 0xFFFF);
+            hexString = (crcValue & 0xFFFF);
+            string retu = Convert.ToString(hexString , 16);
+            
+           
+
+            return  retu;
+        }
+
+      
+    }
+
+}
